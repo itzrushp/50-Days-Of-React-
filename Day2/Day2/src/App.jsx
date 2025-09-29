94c4865f3f24015948be01a090b993a42e548ca0
Rush <itzashert@gmail.com>
2025-09-29 20:42:21 +0530

    Task 2 of using event Handler is Done in the App.jsx itself with buttons and Paras

--- Day2/Day2/src/App.jsx
+++ Day2/Day2/src/App.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
+import React from "react";
 // import './index.css'
-import StyledCard from './components/1.Styles&IconTask/StyledCard'
-import ProfileCardIcon from './components/1.Styles&IconTask/ProfileCardIcon'
-import IconComponent from './components/1.Styles&IconTask/IconComponent.jsx'
+// import StyledCard from './components/1.Styles&IconTask/StyledCard'
+// import ProfileCardIcon from './components/1.Styles&IconTask/ProfileCardIcon'
+// import IconComponent from './components/1.Styles&IconTask/IconComponent.jsx'
+
+// const Button = ()=>{
+//   const ClickHandler = ()=>{
+//     console.log('Ahh....')
+//   }
+
+//   return (
+//     <>
+//     {/*  Just pass in the refrence of the function not calling it */}
+//     <button onClick={ClickHandler}>Click me</button>
+//     </>
+//   )
+// }
+
+// const Paragraph = ()=>{
+//   const copyHandler = ()=>{
+//     alert("You are not allowed to copy this text!!! bitch sybau")
+//   }
+//   return (
+//     <>
+//     <p onCopy={copyHandler}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nostrum, eaque dolore officiis sapiente consequuntur molestias officia voluptatem cupiditate laborum aliquid! Provident architecto et adipisci vero suscipit nulla libero eligendi.</p>
+//     </>
+//   )
+// }
 
 const App = () => {
   return (
     <>
-
-      {/*      Styles and Icone Task 1    */}
+      {/*      Styles and Icone Task 1   
       <StyledCard/>
       <ProfileCardIcon/>
-      <IconComponent/>
+      <IconComponent/> 
+      */}
+
+      {/* Event Handlers Operations Task2
+      <Button/>
+      <Paragraph/> 
+      */}
+
+
+      
     </>
-  )
-}
+  );
+};
 
 export default App;
